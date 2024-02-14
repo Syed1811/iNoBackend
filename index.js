4efffb8318fbdac34625c57fd8da0239e5035110
Syed Raiyanullah <100852456+Syed1811@users.noreply.github.com>
2024-02-14 13:40:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ console.log("Port:", port);
 
 connectToMongo()
   .then(() => {
-    app.use(cors({ origin: "http://localhost:3000" }));
+    app.use(cors({ origin: "https://main--inooteboook.netlify.app/" }));
     app.use(express.json());
     app.use(express.static(path.join(__dirname, "../build")));
 
